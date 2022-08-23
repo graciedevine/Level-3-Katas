aa80cf54ad4221c1396d5124d1a11d61ffb14faf
Gracie <68759643+graciedevine@users.noreply.github.com>
2022-08-23 11:43:18 -0400

    push values to empty array; for loops

---
+++ reversedSequences.js
@@ -0,0 +1,16 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+// Example : n=5 --> [5,4,3,2,1]
+
+//start with empty array
+//start at 5, as long as n > 0, go down 1
+//push 5 and each decresing value into array
+
+const reverseSeq = n => {
+    let arr = [];
+    for (let i = n; i > 0; i--) {
+        arr.push[i];
+        return arr;
+    }
+};
+
+reverseSeq(5)
