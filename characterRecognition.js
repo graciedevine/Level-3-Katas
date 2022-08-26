5cdf5c013a66d8c347ae5d41ca0192d2be9ee040
Gracie <68759643+graciedevine@users.noreply.github.com>
2022-08-26 16:44:18 -0400

    regex /_/g; string.replace()

---
+++ characterRecognition.js
@@ -0,0 +1,11 @@
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+//     S is misinterpreted as 5
+//     O is misinterpreted as 0
+//     I is misinterpreted as 1
+
+function correct(string) {
+    console.log(string.replace(/5/g, "S").replace(/0/g, "O").replace(/1/g, "I"))
+}
+
+correct("L0ND0N")
