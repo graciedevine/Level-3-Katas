e001fdc7fd460995887bd5c0a151b38c9a1da08e
Gracie <68759643+graciedevine@users.noreply.github.com>
2022-08-30 10:05:05 -0400

    array methods, for loop

--- reversedSequences.js
+++ reversedSequences.js
@@ -8,9 +8,9 @@
 const reverseSeq = n => {
     let arr = [];
     for (let i = n; i > 0; i--) {
-        arr.push[i];
+        arr.push(i);
+        }
         return arr;
-    }
 };
 
 reverseSeq(5)
